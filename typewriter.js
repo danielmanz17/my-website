6283e0054eb495c2bbd68a1a11bcb27f5ccea9ab
Daniel Manz <d.manz0620231@arts.ac.uk>
2025-06-14 15:33:33 +0100

    Updated and cleaned

--- typewriter.js
+++ typewriter.js
@@ -1,3 +1,5 @@
+// Type write animation effect for Proust quote
+
 const text = "What higher, truer note must all have struck when the assembled people made their response to the priest, and bowed when the bell sounded for the Elevation, not with the detachment of actors in a revival, but because they, too, like their priest, like the man who had carved the stone, had faith. — Marcel Proust, The Death of the Cathedrals ✟";
 const container = document.getElementById("proust-animated");
 let index = 0;
